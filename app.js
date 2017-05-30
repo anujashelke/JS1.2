428045cca4c5a1a5d96cfb2abf71a8751a115271
anujashelke <anuja_shelke@outlook.com>
2017-05-30 15:36:20 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,9 @@
+var Employee = {"name" : "Anuja", "age" : 25, "DOB" : "7/9/1991", "BirthPlace" : "Amravati" };
+
+document.write("NAME :" + Employee.name + "<br />");
+
+document.write("DOB :" + Employee.age + "<br />");
+
+document.write("DOB :" + Employee.DOB + "<br />");
+
+document.write("Place of Birth :" + Employee.BirthPlace + "<br />");
